1ded8608bdfc20024aad470377984539127e9911
Shrestha Shashank <97455610+Shresth72@users.noreply.github.com>
2023-06-20 06:31:12 +0530

    Update projects.ts

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -1,35 +1,34 @@
-import proj1 from "../../public/projects/1.png";
-import canvas from "../../public/canvas.png";
+
 
 export const projectDetails = [
   {
     id: 1,
     title: "APP",
-    img: proj1,
+    img: "../../public/projects/1.png",
   },
   {
     id: 2,
     title: "WEBAPP",
-    img: canvas,
+    img: "../../public/canvas.png",
   },
   {
     id: 3,
     title: "CODEPEN",
-    img: proj1,
+    img: "../../public/projects/1.png",
   },
   {
     id: 4,
     title: "OPENAI",
-    img: canvas,
+    img: "../../public/canvas.png",
   },
   {
     id: 5,
     title: "THREE",
-    img: proj1,
+    img: "../../public/projects/1.png",
   },
   {
     id: 6,
     title: "REACT",
-    img: canvas,
+    img: "../../public/canvas.png",
   },
 ];
