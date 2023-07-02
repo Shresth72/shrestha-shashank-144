bcc4db68d97741885060a5a16cf99fe935097764
bob <97455610+Shresth72@users.noreply.github.com>
2023-07-02 08:13:26 +0530

    Metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "@/components/HomeComps/Hero";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Portfolio Website | HomePage",
+  title: "Shrestha's Portfolio | HomePage",
   description: "Portfolio Website of Shrestha Shashank",
 };
 
