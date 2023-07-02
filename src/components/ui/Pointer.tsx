2c45d6c106dafe70a7866fe5a66d91772bfd5aec
bob <97455610+Shresth72@users.noreply.github.com>
2023-07-02 06:37:36 +0530

    pack

--- src/components/ui/Pointer.tsx
+++ src/components/ui/Pointer.tsx
@@ -1,15 +1,16 @@
-import AnimatedCursor from "react-animated-cursor";
+// import AnimatedCursor from "react-animated-cursor";
 
 const Pointer = ({}) => {
   return (
-    <AnimatedCursor
-      innerSize={9}
-      color="255,255,255"
-      outerSize={8}
-      innerStyle={{
-        mixBlendMode: "exclusion",
-      }}
-    />
+    // <AnimatedCursor
+    //   innerSize={9}
+    //   color="255,255,255"
+    //   outerSize={8}
+    //   innerStyle={{
+    //     mixBlendMode: "exclusion",
+    //   }}
+    // />
+    <div></div>
   );
 };
 
