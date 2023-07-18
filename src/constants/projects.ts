3347c6234e5763ac37da0fa64890ecfee9c56fbf
bob <97455610+Shresth72@users.noreply.github.com>
2023-07-19 02:17:14 +0530

    project updates

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -4,19 +4,22 @@ import three from "../../public/ThreeJs.png";
 import codepen from "../../public/codepen2.png";
 import behance from "../../public/Behance.png";
 import chaos from "../../public/Chaos.png";
+import motion from "../../public/motion.png";
+import app from "../../public/app.png";
 
 export const projectDetails = [
   {
     id: 1,
-    title: "APP",
-    img: hero,
-    link: "/",
+
+    title: "MOTION",
+    img: motion,
+    link: "https://number-scape.vercel.app",
   },
   {
     id: 2,
-    title: "MOTION",
-    img: canvas,
-    link: "/",
+    title: "THREE",
+    img: three,
+    link: "https://three-fiber-customizer.vercel.app",
   },
   {
     id: 3,
@@ -32,13 +35,12 @@ export const projectDetails = [
   },
   {
     id: 5,
-    title: "THREE",
-    img: three,
-    link: "https://three-fiber-customizer.vercel.app",
+    title: "APP",
+    img: app,
+    link: "https://smart-skin-care-next.vercel.app/",
   },
   {
     id: 6,
-
     title: "BEHANCE",
     img: behance,
     link: "https://www.behance.net/shrivassneha",
